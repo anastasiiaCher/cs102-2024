6bb8f980d0510e91fdeaca396eeaae28d3545475
zalof <zalof299@gmail.com>
2024-09-24 20:02:14 +0300

    Added like 4 new lines

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,2 +1,8 @@
-def text():
+"""This is a module docstring
+it does nothing really."""
+
+
+def text(message):
+    """This is a function docstring
+    neither does that do anything."""
     return message
