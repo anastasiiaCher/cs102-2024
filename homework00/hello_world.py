39184fb1a01fa8a5694b336295b555d7494768cb
zalof <zalof299@gmail.com>
2024-09-30 23:04:08 +0300

    fixed the error (unknown var)

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -2,7 +2,8 @@
 it does nothing really."""
 
 
-def text(message):
+def text():
     """This is a function docstring
     neither does that do anything."""
+    message = "this is a callback message"
     return message
