afc3ce368af4e1f11982a76e99515ffc9593d121
Katharine <eadcypa@gmail.com>
2024-10-08 19:41:53 +0300

    Исправлены ошибки

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,2 +1,7 @@
-def text():
+"""Изначально функция не принимала переменную message, и ей нечего было return"""
+def text(message):
+    """Args:
+        message(str): введённое сообщение
+    Returns:
+        message"""
     return message
