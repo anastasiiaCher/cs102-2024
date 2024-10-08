b73c2ca8aff637c42deb9d8816fec8b484b82991
= <arpo@MacBook-Pro-Artemij.local>
2024-10-08 18:29:44 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -11,7 +11,24 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    shift %= 26
+    for i in plaintext:
+        if i.isalpha() == False:
+            ciphertext += i
+            continue
+        if i.isupper() == True:
+            if (ord(i) + shift) > 90:
+                #print(chr((ord(i) + shift) % 90 + 64))
+                ciphertext += chr((ord(i) + shift) % 90 + 64)
+            else:
+                #print(chr((ord(i) + shift) % 90), i, ord(i) + shift)
+                ciphertext += chr(ord(i) + shift)
+        else:
+            if (ord(i) + shift) > 122:
+                ciphertext += chr((ord(i) + shift) % 122 + 96)
+            else:
+                ciphertext += chr(ord(i) + shift)
+
     return ciphertext
 
 
@@ -28,5 +45,25 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    shift %= 26
+    for i in ciphertext:
+        if i.isalpha() == False:
+            plaintext += i
+            continue
+        if i.isupper() == True:
+            if (ord(i) - shift) < 65:
+                #print(chr(90 - (shift - ord(i) + 64)))
+                plaintext += chr(90 - (shift - ord(i) + 64))
+            else:
+                #print(chr(ord(i) - shift), i, ord(i) + shift)
+                plaintext += chr(ord(i) - shift)
+        else:
+            if (ord(i) - shift) < 97:
+                plaintext += chr(122 - (shift - ord(i) + 96))
+            else:
+                # print(chr((ord(i) + shift) % 90), i, ord(i) + shift)
+                plaintext += chr(ord(i) - shift)
+
     return plaintext
+
+#print(encrypt_caesar("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 15))
