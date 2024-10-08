a4780ac56fb06356f1cfde804dbd371b8be2ca06
sonya <sophia.grachova@gmail.com>
2024-10-08 19:37:22 +0300

    Реализовано шифрование Виженера

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,3 +1,8 @@
+"""
+Encrypts and decrypts a text using Vigenere cipher.
+"""
+
+
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -9,7 +14,23 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i, symbol in enumerate(plaintext):
+        if symbol.isalpha() and symbol.isupper():
+            shift = ord(keyword[i % len(keyword)].upper()) - 65
+            ciphertext += (
+                chr(ord(symbol) + shift)
+                if ord(symbol) + shift < 91
+                else chr(ord(symbol) + shift - 26)
+            )
+        elif symbol.isalpha() and symbol.islower():
+            shift = ord(keyword[i % len(keyword)].lower()) - 97
+            ciphertext += (
+                chr(ord(symbol) + shift)
+                if ord(symbol) + shift < 123
+                else chr(ord(symbol) + shift - 26)
+            )
+        else:
+            ciphertext += symbol
     return ciphertext
 
 
@@ -24,5 +45,21 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i, symbol in enumerate(ciphertext):
+        if symbol.isalpha() and symbol.isupper():
+            shift = ord(keyword[i % len(keyword)].upper()) - 65
+            plaintext += (
+                chr(ord(symbol) - shift)
+                if ord(symbol) - shift > 64
+                else chr(ord(symbol) - shift + 26)
+            )
+        elif symbol.isalpha() and symbol.islower():
+            shift = ord(keyword[i % len(keyword)].lower()) - 97
+            plaintext += (
+                chr(ord(symbol) - shift)
+                if ord(symbol) - shift > 96
+                else chr(ord(symbol) - shift + 26)
+            )
+        else:
+            plaintext += symbol
     return plaintext
