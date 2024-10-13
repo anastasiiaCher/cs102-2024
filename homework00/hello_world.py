f4717c32a4f03d6ad4146fccf605fd15860ca668
ilonabondar <ilona_bondars@mail.ru>
2024-10-13 14:14:19 +0300

    Исправлены ошибки

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,2 +1,16 @@
-def text():
+"""
+This module contains a function that returns the input message.
+"""
+
+
+def text(message):
+    """
+    Returns the input message.
+
+    Parameters:
+    message (str): The message to return.
+
+    Returns:
+    str: The same message that was input.
+    """
     return message
