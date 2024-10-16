0e5f635e05e21f62f34d2acfb700f78f8fb158ae
Katharine-Posh <132606758+Katharine-Posh@users.noreply.github.com>
2024-10-17 00:20:51 +0300

    Update test_hello_world.py

--- homework00/test_hello_world.py
+++ homework00/test_hello_world.py
@@ -6,5 +6,5 @@ import hello_world
 class HelloTestCase(unittest.TestCase):
     def test_hello(self):
         m = "message"
-        self.assertEqual(m, hello_world.text(message))
+        self.assertEqual(m, hello_world.text())
         
