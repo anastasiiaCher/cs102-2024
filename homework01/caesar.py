92834f9e8d7e8278bd313090e1d53d8321ce7c22
amina339 <unicorn23.ru@gmail.com>
2024-10-18 16:15:17 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -11,7 +11,19 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in plaintext:
+        if i.isalpha() and i.isupper():
+            if ord(i) + shift > ord('Z'):
+                ciphertext += chr(ord(i) + (shift - 26))
+            else:
+                ciphertext += chr(ord(i) + shift)
+        elif i.isalpha() and i.islower():
+            if ord(i) + shift > ord('z'):
+                ciphertext += chr(ord(i) + (shift - 26))
+            else:
+                ciphertext += chr(ord(i) + shift)
+        elif not(i.isalpha()):
+            ciphertext += i
     return ciphertext
 
 
@@ -28,5 +40,17 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i in ciphertext:
+        if i.isalpha() and i.isupper():
+            if ord(i) - shift < ord('A'):
+                plaintext += chr(ord(i) + (26 - shift))
+            else:
+                plaintext += chr(ord(i) - shift)
+        elif i.isalpha() and i.islower():
+            if ord(i) - shift < ord('a'):
+                plaintext += chr(ord(i) + (26 - shift))
+            else:
+                plaintext += chr(ord(i) - shift)
+        elif not(i.isalpha()):
+            plaintext += i
     return plaintext
