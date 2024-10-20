3b1ffff9334b57996ba85f80ed1a0b074d3ebe56
NastyaRozhnova <nastyarozhnova@icloud.com>
2024-10-21 02:54:37 +0300

    Реализованы функции encrypt_vigenere() и decrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,3 +1,6 @@
+"""Модуль для работы с шифром Виженера"""
+
+
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -9,7 +12,17 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword_full = (keyword * ((len(plaintext) // len(keyword)) + 1))[: len(plaintext)]
+    for char, key_char in zip(plaintext, keyword_full):
+        if char.isalpha():  # Проверяем, является ли символ буквой
+            shift = ord(key_char.upper()) - ord("A")  # Определяем сдвиг для текущего символа ключа
+            base = (
+                ord("A") if char.isupper() else ord("a")
+            )  # Определяем базовый код: 'A' для заглавных и 'a' для строчных букв
+            new_char = chr((ord(char) - base + shift) % 26 + base)  # Шифруем символ и добавляем его в результат
+            ciphertext += new_char
+        else:
+            ciphertext += char  # Если символ не буква, добавляем его в результат без изменений
     return ciphertext
 
 
@@ -24,5 +37,15 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword_full = (keyword * ((len(ciphertext) // len(keyword)) + 1))[: len(ciphertext)]
+    for char, key_char in zip(ciphertext, keyword_full):
+        if char.isalpha():  # Проверяем, является ли символ буквой
+            shift = ord(key_char.upper()) - ord("A")  # Определяем сдвиг для текущего символа ключа
+            base = (
+                ord("A") if char.isupper() else ord("a")
+            )  # Определяем базовый код: 'A' для заглавных и 'a' для строчных букв
+            new_char = chr((ord(char) - base - shift) % 26 + base)  # Расшифровываем символ и добавляем его в результат
+            plaintext += new_char
+        else:
+            plaintext += char  # Если символ не буква, добавляем его в результат без изменений
     return plaintext
