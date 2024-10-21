1562acb6ccd092716f3cb0a50790b2abd523e8dd
tolkts <zalof299+unigithub@gmail.com>
2024-10-21 23:07:21 +0300

    Finised multiplicative_inverse and generate_keypair

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -12,11 +12,13 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    for i in range(2, int((n ** 0.5) + 1)):
+    if n < 2:
+        return False
+
+    for i in range(2, int((n**0.5) + 1)):
         if n % i == 0:
             return False
-    else:
-        return True
+    return True
 
 
 def gcd(a: int, b: int) -> int:
@@ -27,11 +29,9 @@ def gcd(a: int, b: int) -> int:
     >>> gcd(3, 7)
     1
     """
-    result = 1
-    for i in range(2, int((max(a, b) ** 0.5) + 1)):
-        if a % i == 0 and b % i == 0:
-            result = i
-    return result
+    while b != 0:
+        a, b = b, a % b
+    return abs(a)
 
 
 def multiplicative_inverse(e: int, phi: int) -> int:
@@ -41,8 +41,19 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     >>> multiplicative_inverse(7, 40)
     23
     """
-    # PUT YOUR CODE HERE
-    pass
+    d = phi
+
+    x0, x1 = 1, 0
+    y0, y1 = 0, 1
+
+    while e > 1:
+        div = e // phi
+        e, phi = phi, e % phi
+
+        x0, x1 = x1, x0 - div * x1
+        y0, y1 = y1, y0 - div * y1
+
+    return x0 % d
 
 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
@@ -52,10 +63,10 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
         raise ValueError("p and q cannot be equal")
 
     # n = pq
-    # PUT YOUR CODE HERE
+    n = p * q
 
     # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
+    phi = (p - 1) * (q - 1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
@@ -88,7 +99,7 @@ def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
     # Unpack the key into its components
     key, n = pk
     # Generate the plaintext based on the ciphertext and key using a^b mod m
-    plain = [chr((char ** key) % n) for char in ciphertext]
+    plain = [chr((char**key) % n) for char in ciphertext]
     # Return the array of bytes as a string
     return "".join(plain)
 
