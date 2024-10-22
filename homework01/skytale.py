51c37d870a8d63f2f01ca8319733eefb53a75858
elkulakova <elizaveta.kulakova@niuitmo.ru>
2024-10-22 23:43:16 +0300

    Внесены корректировки в функцию decrypt_skytale()

--- homework01/skytale.py
+++ homework01/skytale.py
@@ -1,6 +1,6 @@
 def decrypt_skytale(plaintext, n):
     """
-    Decrypting Skytale's cipher
+    Decrypts a ciphertext using a Skytale cipher.
     >>> decrypt_skytale("A    LWTEWLEHNE'LADLSLTSL", 5)
     "ALL'S WELL THAT ENDS WELL"
     >>> decrypt_skytale("ACDTKATAWATN", 4)
@@ -8,36 +8,24 @@ def decrypt_skytale(plaintext, n):
     >>> decrypt_skytale("РНОАЫЙКЕСЕ_КТВА", 5)
     'РАКЕТНЫЕ ВОЙСКА'
     """
-    div_text = []
-    text = plaintext
+    table = []
+    text = plaintext.replace("_", " ")
 
     m = len(plaintext) // n
     r = len(text) % m
 
     for col in range(n):
         if col < r:
-            div_text.append(text[: m + 1])
+            table.append(text[: m + 1])
             text = text[m + 1 :]
         else:
-            div_text.append(text[:m] + "_")
+            table.append(text[:m] + " ")
             text = text[m:]
 
     dec_text = ""
     for i in range(m + 1):
-        for j in div_text:
+        for j in table:
             a = j[i]
-            if (
-                ord("А") <= ord(a) <= ord("Я")
-                or ord("A") <= ord(a) <= ord("Z")
-                or ord("a") <= ord(a) <= ord("z")
-                or ord("а") <= ord(a) <= ord("я")
-                or ord(a) == ord("Ё")
-                or ord(a) == ord("ё")
-            ):
-                dec_text += a
-            elif ord(a) == ord("_") or ord(a) == ord(" "):
-                dec_text += " "
-            else:
-                dec_text += a
+            dec_text += a
 
-    return str(dec_text.strip())
+    return dec_text.strip()
