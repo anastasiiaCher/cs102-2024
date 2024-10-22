6b4b148ab85ad77876c36bdf846849eec2727be8
tolkts <zalof299+unigithub@gmail.com>
2024-10-22 23:50:15 +0300

    Made atbash.py  neaat✨

--- homework01/atbash.py
+++ homework01/atbash.py
@@ -1,12 +1,18 @@
+"""
+Atbash module
+"""
+
+
 def encrypt_atbash(plaintext: str):
+    """
+    Encrypts text intp Atbash cipher
+    """
     ciphertext = ""
     for symbol in plaintext:
         if symbol.isalpha():
-            gap = ord(symbol) - ord("a" if symbol.islower() else "A") 
+            gap = ord(symbol) - ord("a" if symbol.islower() else "A")
             ciphertext += chr(ord("z" if symbol.islower() else "Z") - gap)
         else:
             ciphertext += symbol
 
     return ciphertext
-
-print(encrypt_atbash("aA zZ bB yY"))
