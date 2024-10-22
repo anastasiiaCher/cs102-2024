c797f3f4b7df3f741386bc98456ab821ad382835
DimaGrach1 <veritech1402@gmail.com>
2024-10-22 18:28:51 +0300

    Реализован Аффинный шифр

---
+++ homework01/extra.py
@@ -0,0 +1,12 @@
+def encrypt_affine(plaintext, a, b):
+    ciphertext = ""
+    for i in plaintext:
+        if i.isalpha():
+            if i.isupper():
+                temp = chr(1040 + (((ord(i) - 16) % 32 * a + b) % 32))
+            else:
+                temp = chr(1072 + ((((ord(i) - 16) % 32) * a + b) % 32))
+            ciphertext += temp
+        else:
+            ciphertext += i
+    return ciphertext
