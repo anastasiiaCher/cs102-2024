0705b26d8beaa4d7b4d4e9ec4d440683ccc43c15
Kamilla <kamilla.rubinshtein@gmail.com>
2024-10-22 19:56:11 +0300

    Шифр Атбаш

---
+++ homework01/Шифр Атбаш (рус).py
@@ -0,0 +1,20 @@
+def encrypt_atbash(plaintext):
+    alfabeto = ("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ")
+    alfabetoatbash=''.join(reversed(alfabeto))
+    alfabetominuscole=alfabeto.lower()
+    alfabetoatbash+=''.join(reversed(alfabetominuscole))
+    alfabeto = alfabeto + alfabetominuscole
+#print(alfabetoatbash)
+    ciphered_text=""
+    numero=0
+
+    for char in plaintext:
+
+        if char.isalpha():
+            numero = alfabeto.index(char)
+            ciphered_text+=alfabetoatbash[numero]
+        else:
+            ciphered_text+=char
+    return ciphered_text
+#print(encrypt_atbash("Привет Мир"))
+
