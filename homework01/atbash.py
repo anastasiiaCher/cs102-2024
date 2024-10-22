8540c40b8893c6036ec9d7a29adbeeecf3d7a586
sonya <sophia.grachova@gmail.com>
2024-10-22 19:31:13 +0300

    Шифр Атбаш

---
+++ homework01/atbash.py
@@ -0,0 +1,17 @@
+"""
+Encrypts and decrypts a text using Atbash cipher.
+"""
+
+def encrypt_atbash(plaintext: str) -> str:
+    """
+    Encrypts the given text.
+    """
+    ciphertext = ""
+    for symbol in plaintext:
+        if symbol.isalpha() and symbol.isupper():
+            ciphertext += chr(ord('А') + ord('Я') - ord(symbol) - 1)
+        elif symbol.isalpha() and symbol.islower():
+            ciphertext += chr(ord('а') + ord('я') - ord(symbol) - 1)
+        else:
+            ciphertext += symbol
+    return ciphertext
