a8bdab7e408ddee5bdfd3069107ce1945efb7fcb
Generalusus <kirill.enderman@gmail.com>
2024-10-24 22:25:47 +0300

    Реализованы функции encrypt_caesar() и decrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,32 +1,43 @@
+"""Caesar cipher system"""
+
+
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    """
-    Encrypts plaintext using a Caesar cipher.
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
+    """Encrypting with C.c.s"""
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for plain in enumerate(plaintext):
+        if plain[1] in alphabet:
+            if ord(plain[1]) in range(97, 123):
+                if ord(plain[1]) + shift > 122:
+                    ciphertext += chr(ord(plain[1]) + shift - 26)
+                else:
+                    ciphertext += chr(ord(plain[1]) + shift)
+            if ord(plain[1]) in range(65, 91):
+                if ord(plain[1]) + shift > 90:
+                    ciphertext += chr(ord(plain[1]) + shift - 26)
+                else:
+                    ciphertext += chr(ord(plain[1]) + shift)
+        else:
+            ciphertext += plain[1]
     return ciphertext
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
-    """
-    Decrypts a ciphertext using a Caesar cipher.
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
+    """Decrypting with C.c.s"""
     plaintext = ""
-    # PUT YOUR CODE HERE
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for cipher in enumerate(ciphertext):
+        if cipher[1] in alphabet:
+            if ord(cipher[1]) in range(97, 123):
+                if ord(cipher[1]) - shift < 97:
+                    plaintext += chr(ord(cipher[1]) - shift + 26)
+                else:
+                    plaintext += chr(ord(cipher[1]) - shift)
+            if ord(cipher[1]) in range(65, 91):
+                if ord(cipher[1]) - shift < 65:
+                    plaintext += chr(ord(cipher[1]) - shift + 26)
+                else:
+                    plaintext += chr(ord(cipher[1]) - shift)
+        else:
+            plaintext += cipher[1]
     return plaintext
