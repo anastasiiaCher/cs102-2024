2940cdd8fb6a1c533e1c63433b5cb64d2142561e
Generalusus <kirill.enderman@gmail.com>
2024-10-24 23:03:27 +0300

    Небольшие изменения

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -7,13 +7,13 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     for plain in enumerate(plaintext):
         if plain[1] in alphabet:
-            if ord(plain[1]) in range(97, 123):
-                if ord(plain[1]) + shift > 122:
+            if ord(plain[1]) in range(ord('a'), ord('z')+1):
+                if ord(plain[1]) + shift > ord('z'):
                     ciphertext += chr(ord(plain[1]) + shift - 26)
                 else:
                     ciphertext += chr(ord(plain[1]) + shift)
-            if ord(plain[1]) in range(65, 91):
-                if ord(plain[1]) + shift > 90:
+            if ord(plain[1]) in range(ord('A'), ord('Z')+1):
+                if ord(plain[1]) + shift > ord('Z'):
                     ciphertext += chr(ord(plain[1]) + shift - 26)
                 else:
                     ciphertext += chr(ord(plain[1]) + shift)
@@ -28,13 +28,13 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     for cipher in enumerate(ciphertext):
         if cipher[1] in alphabet:
-            if ord(cipher[1]) in range(97, 123):
-                if ord(cipher[1]) - shift < 97:
+            if ord(cipher[1]) in range(ord('a'), ord('z')+1):
+                if ord(cipher[1]) - shift < ord('a'):
                     plaintext += chr(ord(cipher[1]) - shift + 26)
                 else:
                     plaintext += chr(ord(cipher[1]) - shift)
-            if ord(cipher[1]) in range(65, 91):
-                if ord(cipher[1]) - shift < 65:
+            if ord(cipher[1]) in range(ord('A'), ord('Z')+1):
+                if ord(cipher[1]) - shift < ord('A'):
                     plaintext += chr(ord(cipher[1]) - shift + 26)
                 else:
                     plaintext += chr(ord(cipher[1]) - shift)
