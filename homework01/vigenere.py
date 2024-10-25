eb7bf82c5fb45073d2ae3d10e2a077c0f661eb94
Ксения Калиева <kseniakalieva@192-168-0-108.local>
2024-10-25 17:26:48 +0300

    vigenere

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -8,9 +8,22 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
+    keyword_length = len(keyword)
     ciphertext = ""
-    # PUT YOUR CODE HERE
+
+    for i, char in enumerate(plaintext):
+        if char.isalpha():
+            shift = ord(keyword[i % keyword_length].lower()) - ord('a')
+            if char.islower():
+                encrypted_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+            else:
+                encrypted_char = chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+            ciphertext += encrypted_char
+        else:
+            ciphertext += char 
+
     return ciphertext
+
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
@@ -23,6 +36,20 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
+    keyword_length = len(keyword)
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i, char in enumerate(ciphertext):
+        if char.isalpha():
+            shift = ord(keyword[i % keyword_length].lower()) - ord('a')
+            if char.islower():
+                decrypted_char = chr((ord(char) - ord('a') - shift) % 26 + ord('a'))
+            else:
+                decrypted_char = chr((ord(char) - ord('A') - shift) % 26 + ord('A'))
+            plaintext += decrypted_char
+        else:
+            plaintext += char 
+
     return plaintext
+
+print(encrypt_vigenere("ATTACKATDAWN", "LEMON"))
+print(decrypt_vigenere("LXFOPVEFRNHR", "LEMON"))
