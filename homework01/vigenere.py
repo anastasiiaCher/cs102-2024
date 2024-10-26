ac5017e0a09807837b8a1d96fd0fa9c8d85c54ab
amina339 <unicorn23.ru@gmail.com>
2024-10-26 21:41:33 +0300

    Реализована функция encrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,7 +9,25 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    k = 0
+    newkeyword = keyword * (len(plaintext) // len(keyword) + 1) if len(keyword) < len(plaintext) else keyword
+    shift = [ord(x.lower()) - ord('a') for x in newkeyword]
+    for i in plaintext:
+        if i.isupper() and i.isalpha():
+            if ord(i) + shift[k] > ord('Z'):
+                ciphertext += chr(ord(i) + (shift[k] - 26))
+            else:
+                ciphertext += chr(ord(i) + shift[k])
+            k += 1
+        elif i.islower() and i.isalpha():
+            if ord(i) + shift[k] > ord('z'):
+                ciphertext += chr(ord(i) + (shift[k] - 26))
+            else:
+                ciphertext += chr(ord(i) + shift[k])
+            k += 1
+        elif not(i.isalpha()):
+            ciphertext += i
+            k += 1
     return ciphertext
 
 
@@ -24,5 +42,23 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    k = 0
+    newkeyword = keyword * (len(ciphertext) // len(keyword) + 1) if len(keyword) < len(ciphertext) else keyword
+    shift = [ord(x.lower()) - ord('a') for x in newkeyword]
+    for i in ciphertext:
+        if i.isupper() and i.isalpha():
+            if ord(i) - shift[k] < ord('A'):
+                plaintext += chr(ord(i) + (26 - shift[k]))
+            else:
+                plaintext += chr(ord(i) - shift[k])
+            k += 1
+        elif i.islower() and i.isalpha():
+            if ord(i) - shift[k] < ord('a'):
+                plaintext += chr(ord(i) + (26 - shift[k]))
+            else:
+                plaintext += chr(ord(i) - shift[k])
+            k += 1
+        elif not(i.isalpha()):
+            plaintext += i
+            k += 1
     return plaintext
