957ec342f327abf1597ddf82991bb22c62c8607b
amina339 <unicorn23.ru@gmail.com>
2024-10-26 22:36:11 +0300

    Реализована функция is_prime(n)

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -12,7 +12,13 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
+    if n <= 0 or n == 1:
+        return False
+    else:
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
     pass
 
 
