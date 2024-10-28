481a73e0a5c553fa089627d136dcc66613183c69
DamianITMO <teststoragee@gmail.com>
2024-10-28 20:54:18 +0300

    На основе Цезаря слепил это

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,4 +1,9 @@
-def encrypt_vigenere(plaintext: str, keyword: str) -> str:
+"""
+Super Vinegere encoding programm
+"""
+
+
+def encrypt_vigenere(plaintext: str, Schlusselwort: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
     >>> encrypt_vigenere("PYTHON", "A")
@@ -9,11 +14,29 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    Schlusselwort = Schlusselwort * ((len(plaintext) // len(Schlusselwort)) + 1)
+    indx = -1
+    for i in plaintext:
+        indx = indx + 1
+        if i.isalpha() is False:
+            ciphertext = ciphertext + i
+            continue
+        if i.isupper() is True:
+            shift = (ord(Schlusselwort[indx]) - 65) % 26
+            if (ord(i) + shift) > 90:
+                ciphertext = ciphertext + chr(((ord(i) + shift) % 90) + 64)
+            else:
+                ciphertext = ciphertext + chr(ord(i) + shift)
+        else:
+            shift = (ord(Schlusselwort[indx]) - 97) % 26
+            if (ord(i) + shift) > 122:
+                ciphertext = ciphertext + chr(((ord(i) + shift) % 122) + 96)
+            else:
+                ciphertext = ciphertext + chr(ord(i) + shift)
     return ciphertext
 
 
-def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
+def decrypt_vigenere(ciphertext: str, Schlusselwort: str) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
     >>> decrypt_vigenere("PYTHON", "A")
@@ -24,5 +47,23 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    Schlusselwort = Schlusselwort * ((len(ciphertext) // len(Schlusselwort)) + 1)
+    indx = -1
+    for i in ciphertext:
+        indx = indx + 1
+        if i.isalpha() is False:
+            plaintext += i
+            continue
+        if i.isupper() is True:
+            shift = (ord(Schlusselwort[indx]) - 65) % 26
+            if (ord(i) - shift) < 65:
+                plaintext = plaintext + (chr(90 - (shift - ord(i) + 64)))
+            else:
+                plaintext = plaintext + (chr(ord(i) - shift))
+        else:
+            shift = (ord(Schlusselwort[indx]) - 97) % 26
+            if (ord(i) - shift) < 97:
+                plaintext = plaintext + (chr(122 - (shift - ord(i) + 96)))
+            else:
+                plaintext = plaintext + (chr(ord(i) - shift))
     return plaintext
