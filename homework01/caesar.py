dad65582da4d756f69f3f4d1314e85247c68f73e
DamianITMO <teststoragee@gmail.com>
2024-10-28 17:46:23 +0300

    Add files via upload

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,3 +1,7 @@
+"""
+decrypt Caesar codes
+"""
+
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
@@ -10,8 +14,21 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     >>> encrypt_caesar("")
     ''
     """
+    i = 0
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in range(len(plaintext)):
+        sim = ord(plaintext[i])
+        if sim < 65 or sim > 90 and sim < 97 or sim > 122:
+            ciphertext = ciphertext + chr(sim)
+        else:
+            if sim <= 90 and (sim + shift) <= 90:
+                ciphertext = ciphertext + chr(sim + shift)
+            if sim <= 90 and (sim + shift) > 90:
+                ciphertext = ciphertext + chr(65 + ((sim + shift) - 91))
+            if sim >= 97 and (sim + shift) > 122:
+                ciphertext = ciphertext + chr(97 + ((sim + shift) - 123))
+            if sim >= 97 and (sim + shift) <= 122:
+                ciphertext = ciphertext + chr(sim + shift)
     return ciphertext
 
 
@@ -28,5 +45,18 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    i = 0
+    for i in range(len(ciphertext)):
+        sim = ord(ciphertext[i])
+        if sim < 65 or sim > 90 and sim < 97 or sim > 122:
+            plaintext = plaintext + chr(sim)
+        else:
+            if sim <= 90 and (sim - shift) >= 65:
+                plaintext = plaintext + chr(sim - (shift))
+            if sim <= 90 and (sim - shift) < 65:
+                plaintext = plaintext + chr(90 - (63 - (sim - shift - 1)))
+            if sim >= 97 and (sim - shift) < 97:
+                plaintext = plaintext + chr(122 - (95 - (sim - shift - 1)))
+            if sim >= 97 and (sim - shift) >= 97:
+                plaintext = plaintext + chr(sim - (shift))
     return plaintext
