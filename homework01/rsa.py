c59d2dbd39a5694a458ac5f42ddc7d535e4da7f6
Juliewh <yuliamaryashina@gmail.com>
2024-10-30 21:01:05 +0300

    Реализована функция is_prime (n)

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -12,8 +12,18 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
-    pass
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
 
 def gcd(a: int, b: int) -> int:
