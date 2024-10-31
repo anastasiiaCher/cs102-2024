6b1130f3091cb8aad36290cbbf0a4e251448f360
Katharine-Posh <132606758+Katharine-Posh@users.noreply.github.com>
2024-10-31 15:48:51 +0300

    Исправлены синтаксические ошибки для теста black

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,5 +1,5 @@
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    '''
+    """
     Encrypts plaintext using a Caesar cipher.
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
@@ -9,20 +9,20 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     'Sbwkrq3.6'
     >>> encrypt_caesar("")
     ''
-    '''
+    """
     ciphertext = ""
     for char in plaintext:
         if char.isalpha():
-            char_code=ord(char)+shift
+            char_code = ord(char) + shift
             if char.islower():
-                if char_code > ord('z'):
+                if char_code > ord("z"):
                     char_code -= 26
             elif char.isupper():
-                if char_code > ord('Z'):
+                if char_code > ord("Z"):
                     char_code -= 26
-            ciphertext+=chr(char_code)
+            ciphertext += chr(char_code)
         else:
-            ciphertext+=char
+            ciphertext += char
     return ciphertext
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
@@ -40,14 +40,14 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     plaintext = ""
     for char in ciphertext:
         if char.isalpha():
-            char_code=ord(char)-shift
+            char_code= ord(char) - shift
             if char.islower():
-                if char_code < ord('a'):
+                if char_code < ord("a"):
                     char_code += 26
             elif char.isupper():
-                if char_code < ord('A'):
+                if char_code < ord("A"):
                     char_code += 26
-            plaintext+=chr(char_code)
+            plaintext += chr(char_code)
         else:
             plaintext+=char
     return plaintext
