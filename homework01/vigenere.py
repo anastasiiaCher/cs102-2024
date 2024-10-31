56a661e19b90687038e7ea1ffe0452ac02f61c70
Arseny 467106 <arpleshnev@yandex.ru>
2024-10-31 12:50:59 +0300

    Implemented vigenere encrypt and decrypt algorithms.

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,3 +1,6 @@
+"""
+Vigenere encrypt and decrypt algorithms.
+"""
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -9,7 +12,26 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.lower()
+    keyword = keyword * (len(plaintext) // (len(keyword)) + 1)
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    if plaintext == "":
+        return plaintext
+
+    for i, letter in enumerate(plaintext):
+        shift = alphabet.index(keyword[i])
+        letter = plaintext[i]
+        oldcode = ord(letter)
+        newcode = oldcode + (shift % 27)
+
+        if 65 <= oldcode <= 90:
+            letter = chr(newcode % 91 + 65) if newcode > 90 else chr(newcode)
+        elif 97 <= oldcode <= 122:
+            letter = chr(newcode % 123 + 97) if newcode > 122 else chr(newcode)
+
+        ciphertext += letter
+
     return ciphertext
 
 
@@ -24,5 +46,24 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.lower()
+    keyword = keyword * (len(ciphertext) // (len(keyword)) + 1)
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    if ciphertext == "":
+        return ciphertext
+
+    for i, letter in enumerate(ciphertext):
+        shift = alphabet.index(keyword[i])
+        letter = ciphertext[i]
+        newcode = ord(letter)
+        oldcode = newcode - (shift % 27)
+
+        if 65 <= newcode <= 90:
+            letter = chr(oldcode + 26) if oldcode < 65 else chr(oldcode)
+        elif 97 <= newcode <= 122:
+            letter = chr(oldcode + 26) if oldcode < 97 else chr(oldcode)
+
+        plaintext += letter
+
     return plaintext
