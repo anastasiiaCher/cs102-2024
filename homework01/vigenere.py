cff4ec052ec46d6b40ae91c2fb0e99b5b8444c80
amina339 <unicorn23.ru@gmail.com>
2024-10-31 15:47:34 +0300

    Исправлены ошибки

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -11,21 +11,21 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     ciphertext = ""
     k = 0
     newkeyword = keyword * (len(plaintext) // len(keyword) + 1) if len(keyword) < len(plaintext) else keyword
-    shift = [ord(x.lower()) - ord('a') for x in newkeyword]
+    shift = [ord(x.lower()) - ord("a") for x in newkeyword]
     for i in plaintext:
         if i.isupper() and i.isalpha():
-            if ord(i) + shift[k] > ord('Z'):
+            if ord(i) + shift[k] > ord("Z"):
                 ciphertext += chr(ord(i) + (shift[k] - 26))
             else:
                 ciphertext += chr(ord(i) + shift[k])
             k += 1
         elif i.islower() and i.isalpha():
-            if ord(i) + shift[k] > ord('z'):
+            if ord(i) + shift[k] > ord("z"):
                 ciphertext += chr(ord(i) + (shift[k] - 26))
             else:
                 ciphertext += chr(ord(i) + shift[k])
             k += 1
-        elif not(i.isalpha()):
+        elif not (i.isalpha()):
             ciphertext += i
             k += 1
     return ciphertext
@@ -44,21 +44,21 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     plaintext = ""
     k = 0
     newkeyword = keyword * (len(ciphertext) // len(keyword) + 1) if len(keyword) < len(ciphertext) else keyword
-    shift = [ord(x.lower()) - ord('a') for x in newkeyword]
+    shift = [ord(x.lower()) - ord("a") for x in newkeyword]
     for i in ciphertext:
         if i.isupper() and i.isalpha():
-            if ord(i) - shift[k] < ord('A'):
+            if ord(i) - shift[k] < ord("A"):
                 plaintext += chr(ord(i) + (26 - shift[k]))
             else:
                 plaintext += chr(ord(i) - shift[k])
             k += 1
         elif i.islower() and i.isalpha():
-            if ord(i) - shift[k] < ord('a'):
+            if ord(i) - shift[k] < ord("a"):
                 plaintext += chr(ord(i) + (26 - shift[k]))
             else:
                 plaintext += chr(ord(i) - shift[k])
             k += 1
-        elif not(i.isalpha()):
+        elif not (i.isalpha()):
             plaintext += i
             k += 1
     return plaintext
