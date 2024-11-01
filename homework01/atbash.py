dfbf973349407baea28588ed8641ff44fa4f4ce4
emopudge <emiliakudelenskaa@gmail.com>
2024-11-01 15:14:01 +0300

    исправлены кавычки

--- homework01/atbash.py
+++ homework01/atbash.py
@@ -1,33 +1,33 @@
 def encrypt_atbash(plaintext):
-    ciphertext = ''
+    ciphertext = ""
     for i in plaintext:
         if i.isalpha():
             if i.islower():
-                if ord(i) <= ord('е'):
-                    ciphertext += chr(ord('я') - ord(i) + ord('а'))
-                elif i == 'ё':
-                    ciphertext += 'щ'
-                elif i == 'ж':
-                    ciphertext += 'ш'
-                elif i == 'щ':
-                    ciphertext += 'ё'
-                elif ord('е') + 1 < ord(i) < ord('щ'):
-                    ciphertext += chr(ord('я') - ord(i) + ord('а') - 1)
+                if ord(i) <= ord("е"):
+                    ciphertext += chr(ord("я") - ord(i) + ord("а"))
+                elif i == "ё":
+                    ciphertext += "щ"
+                elif i == "ж":
+                    ciphertext += "ш"
+                elif i == "щ":
+                    ciphertext += "ё"
+                elif ord("е") + 1 < ord(i) < ord("щ"):
+                    ciphertext += chr(ord("я") - ord(i) + ord("а") - 1)
                 else:
-                    ciphertext += chr(ord('я') - ord(i) + ord('а'))
+                    ciphertext += chr(ord("я") - ord(i) + ord("а"))
             else:
-                if ord(i) <= ord('Е'):
-                    ciphertext += chr(ord('Я') - ord(i) + ord('А'))
-                elif i == 'Ё':
-                    ciphertext += 'Щ'
-                elif i == 'Ж':
-                    ciphertext += 'Ш'
-                elif i == 'Щ':
-                    ciphertext += 'Ё'
-                elif ord('Е') + 1 < ord(i) < ord('Щ'):
-                    ciphertext += chr(ord('Я') - ord(i) + ord('А') - 1)
+                if ord(i) <= ord("Е"):
+                    ciphertext += chr(ord("Я") - ord(i) + ord("А"))
+                elif i == "Ё":
+                    ciphertext += "Щ"
+                elif i == "Ж":
+                    ciphertext += "Ш"
+                elif i == "Щ":
+                    ciphertext += "Ё"
+                elif ord("Е") + 1 < ord(i) < ord("Щ"):
+                    ciphertext += chr(ord("Я") - ord(i) + ord("А") - 1)
                 else:
-                    ciphertext += chr(ord('Я') - ord(i) + ord('А'))
+                    ciphertext += chr(ord("Я") - ord(i) + ord("А"))
         else:
             ciphertext += i
     return ciphertext
