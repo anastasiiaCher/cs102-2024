2cd32a9c0d70e7d9a27b7074e471f711d06e2850
emopudge <emiliakudelenskaa@gmail.com>
2024-11-01 15:14:28 +0300

    исправлены кавычки

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,13 +1,13 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    # """
-    # Encrypts plaintext using a Vigenere cipher.
-    # >>> encrypt_vigenere("PYTHON", "A")
-    # 'PYTHON'
-    # >>> encrypt_vigenere("python", "a")
-    # 'python'
-    # >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    # 'LXFOPVEFRNHR'
-    # """
+    """
+    Encrypts plaintext using a Vigenere cipher.
+    >>> encrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> encrypt_vigenere("python", "a")
+    'python'
+    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
+    'LXFOPVEFRNHR'
+    """
 
     if len(plaintext) > len(keyword):
         diff = len(plaintext) - len(keyword)
@@ -43,15 +43,15 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
+    """
+    Decrypts a ciphertext using a Vigenere cipher.
+    >>> decrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> decrypt_vigenere("python", "a")
+    'python'
+    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
+    'ATTACKATDAWN'
     # """
-    # Decrypts a ciphertext using a Vigenere cipher.
-    # >>> decrypt_vigenere("PYTHON", "A")
-    # 'PYTHON'
-    # >>> decrypt_vigenere("python", "a")
-    # 'python'
-    # >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    # 'ATTACKATDAWN'
-    # # """
 
     if len(ciphertext) > len(keyword):
         diff = len(ciphertext) - len(keyword)
