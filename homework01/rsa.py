8ac1c9e027fad23467252fe871c3e4a204240ee0
emopudge <133000209+emopudge@users.noreply.github.com>
2024-11-01 16:34:43 +0300

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -33,7 +33,7 @@ def gcd(a: int, b: int) -> int:
     >>> gcd(3, 7)
     1
     """
-    m=0
+    m = 0
     for i in range(1, max(a, b) + 1):
         if a % i == 0 and b % i == 0 and i > m:
             m = i
