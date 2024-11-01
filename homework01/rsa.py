3b7399e2edde0ccf1a20f74248fa72c3e96b6e55
Katharine-Posh <132606758+Katharine-Posh@users.noreply.github.com>
2024-11-01 17:30:24 +0300

    Опять исправлены ошибки для проверки black

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -63,7 +63,7 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
         raise ValueError("p and q cannot be equal")
 
     n = p * q
-    phi = (p-1) * (q-1)
+    phi = (p - 1) * (q - 1)
     
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
@@ -87,7 +87,7 @@ def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
     key, n = pk
     # Convert each letter in the plaintext to numbers based on
     # the character using a^b mod m
-    cipher = [(ord(char)**key) % n for char in plaintext]
+    cipher = [(ord(char) ** key) % n for char in plaintext]
     # Return the array of bytes
     return cipher
 
