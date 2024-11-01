8cd9975da06dd7b4cf3146f68c6d0f4b9758ae05
emopudge <emiliakudelenskaa@gmail.com>
2024-11-01 15:14:19 +0300

    исправлены кавычки

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,26 +1,25 @@
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
-
     >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
+    "SBWKRQ"
     >>> encrypt_caesar("python")
-    'sbwkrq'
+    "sbwkrq"
     >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
+    "Sbwkrq3.6"
     >>> encrypt_caesar("")
-    ''
+    ""
     """
     ciphertext = ""
     for char in plaintext:
         if char.isalpha():
             if char.islower():
-                if ord(char) < ord('z') - shift + 1:
+                if ord(char) < ord("z") - shift + 1:
                     ciphertext += chr(ord(char) + shift)
                 else:
                     ciphertext += chr(ord(char) + shift - 26)
             else:
-                if ord(char) < ord('Z') - shift + 1:
+                if ord(char) < ord("Z") - shift + 1:
                     ciphertext += chr(ord(char) + shift)
                 else:
                     ciphertext += chr(ord(char) + shift - 26)
@@ -34,19 +33,19 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     Decrypts a ciphertext using a Caesar cipher.
 
     >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
+    "PYTHON"
     >>> decrypt_caesar("sbwkrq")
-    'python'
+    "python"
     >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
+    "Python3.6"
     >>> decrypt_caesar("")
-    ''
+    ""
     """
     plaintext = ""
     for char in ciphertext:
         if char.isalpha():
             if char.islower():
-                if ord(char) > ord('a') + shift - 1:
+                if ord(char) > ord("a") + shift - 1:
                     plaintext += chr(ord(char) - shift)
                 else:
                     plaintext += chr(ord(char) - shift + 26)
