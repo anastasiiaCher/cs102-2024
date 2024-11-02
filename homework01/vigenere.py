80048cdd7134bd7109889019123559ee3bff3104
Baha <bahaahmedov@Bahas-MacBook-Air.local>
2024-11-02 13:31:58 +0300

    Реализована функция encrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,7 +9,23 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+
+    alphabet = list("abcdefghijklmnopqrstuvwxyz")
+    keyword_ides = list(keyword)
+    text = list(plaintext)
+
+    for i in range(len(keyword_ides)):
+        if keyword_ides[i].isalpha():
+            keyword_ides[i] = alphabet.index(keyword_ides[i].lower())
+
+    for i in range(len(text)):
+        if text[i].isalpha():
+            base = ord('A') if text[i].isupper() else ord('a')
+            shift = (alphabet.index(text[i].lower()) + keyword_ides[i % len(keyword_ides)]) % 26
+            ciphertext += chr(base + shift)
+        else:
+            ciphertext += text[i]
+
     return ciphertext
 
 
