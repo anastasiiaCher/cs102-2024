b570e6e41dfeb0fcb52039a3890c09cc84bf2cfd
sonya <sophia.grachova@gmail.com>
2024-11-05 08:25:19 +0300

    Return added

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -23,7 +23,7 @@ def is_prime(n: int) -> bool:
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
-        return True
+    return True
 
 
 def gcd(a: int, b: int) -> int:
