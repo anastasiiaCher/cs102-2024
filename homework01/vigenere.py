4d2081294df546e830fdc19be9ec1cf8bfa1f2ae
Astmatik001 <esoklakov@gmail.com>
2024-11-06 10:29:33 +0300

    encrypt_vigenere() function is implemented

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,7 +9,32 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    int_key = []
+
+    for index, char in enumerate(keyword):
+        if (96 < ord(char) < 123):
+            int_key.append(ord(char) - 97)
+        elif(64 < ord(char) < 91):
+            int_key.append(ord(char) - 65)
+
+    for index, char in enumerate(plaintext):
+        shift = int_key[index % len(int_key)]
+        old_int = ord(char)
+        overflow = 0
+        
+        if( 96 < old_int < 123):
+            if old_int + shift > 122:
+                overflow = -26
+            new_char = chr(old_int + shift + overflow)
+        elif( 64 < old_int < 91):
+            if old_int + shift > 90:
+                overflow = -26
+            new_char = chr(old_int + shift + overflow)
+        else:
+            new_char = char
+
+        ciphertext = ciphertext + new_char
+
     return ciphertext
 
 
