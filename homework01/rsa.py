0cf33caca4ffb548daf14379344fc274c8a77470
lizausenko <lizausenko2001@mail.ru>
2024-11-08 18:19:55 +0300

    Исправлены ошибки

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -12,13 +12,12 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-        else:
-            continue
-    return True
-    pass
+    if n > 1:
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+        return True
+    return False
 
 
 def gcd(a: int, b: int) -> int:
@@ -43,19 +42,11 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     23
     """
 
-    def extended_gcd(a, b):
-        if not a:
-            return b, 0, 1
-        gcd, x1, y1 = extended_gcd(b % a, a)
-        x = y1 - (b // a) * x1
-        y = x1
-        return gcd, x, y
-
-        gcd, x, y = extended_gcd(e, phi)
-        if gcd != 1:
-            return "Обратного элемента не существует"
-        else:
-            return x % phi
+    d = 0
+    for i in range(1, phi):
+        if i * e % phi == 1:
+            d = i
+    return d
 
     pass
 
@@ -66,10 +57,10 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
     elif p == q:
         raise ValueError("p and q cannot be equal")
 
-    # n = pq
+    n = p * q
     # PUT YOUR CODE HERE
 
-    # phi = (p-1)(q-1)
+    phi = (p - 1) * (q - 1)
     # PUT YOUR CODE HERE
 
     # Choose an integer e such that e and phi(n) are coprime
@@ -103,7 +94,7 @@ def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
     # Unpack the key into its components
     key, n = pk
     # Generate the plaintext based on the ciphertext and key using a^b mod m
-    plain = [chr((char**key) % n) for char in ciphertext]
+    plain = [chr((char ** key) % n) for char in ciphertext]
     # Return the array of bytes as a string
     return "".join(plain)
 
--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,19 +9,25 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    for char in range(len(plaintext)):
-        if plaintext[char].isupper():
-            new_ord = ord(plaintext[char]) + (ord(keyword[char % len(keyword)]) - ord("A"))
-            if new_ord > ord("Z"):
-                new_ord -= 26
-            ciphertext += chr(new_ord)
-
+    repeats = (len(plaintext) // len(keyword) + 1) * keyword
+    rez = repeats[: len(plaintext)]
+    index = 0
+    for i in plaintext:
+        if ord("a") > ord(i.lower()) or ord(i.lower()) > ord("z"):
+            ciphertext += i
+            index += 1
         else:
-            new_ord = ord(plaintext[char]) + (ord(keyword[char % len(keyword)]) - ord("a"))
-            if new_ord > ord("z"):
-                new_ord -= 26
-            ciphertext += chr(new_ord)
-
+            k = rez[index]
+            shift = ord(k.lower()) - ord("a")
+            if ord(i.lower()) + shift <= ord("z"):
+                index += 1
+                ciphertext += chr(ord(i) + shift)
+            else:
+                if i.isupper():
+                    ciphertext += (chr(ord("a") + (ord(i.lower()) + shift - ord("z")) - 1)).upper()
+                if i.islower():
+                    ciphertext += (chr(ord("a") + (ord(i) + shift - ord("z")) - 1)).lower()
+                index += 1
     return ciphertext
 
 
@@ -36,18 +42,23 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-
-    for char in range(len(ciphertext)):
-        if ciphertext[char].isupper():
-            new_ord = ord(ciphertext[char]) - (ord(keyword[char % len(keyword)]) - ord("A"))
-            if new_ord < ord("A"):
-                new_ord += 26
-            plaintext += chr(new_ord)
-
+    repeats = (len(ciphertext) // len(keyword) + 1) * keyword
+    rez = repeats[: len(ciphertext)]
+    index = 0
+    for i in ciphertext:
+        if ord("a") > ord(i.lower()) or ord(i.lower()) > ord("z"):
+            plaintext += i
+            index += 1
         else:
-            new_ord = ord(ciphertext[char]) + (ord(keyword[char % len(keyword)]) - ord("a"))
-            if new_ord < ord("a"):
-                new_ord += 26
-            plaintext += chr(new_ord)
-
+            k = rez[index]
+            shift = ord(k.lower()) - ord("a")
+            if ord(i.lower()) - shift >= ord("a"):
+                index += 1
+                plaintext += chr(ord(i) - shift)
+            else:
+                if i.isupper():
+                    plaintext += (chr(ord(i) - ord("a") + ord("z") - shift + 1)).upper()
+                if i.islower():
+                    plaintext += (chr(ord(i) - ord("a") + ord("z") - shift + 1)).lower()
+                index += 1
     return plaintext
