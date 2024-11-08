fd5a9e526ac543a648536ce54ff38a37ea3778b4
lizausenko <lizausenko2001@mail.ru>
2024-11-08 19:03:07 +0300

    vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,7 +9,6 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-<<<<<<< HEAD
     repeats = (len(plaintext) // len(keyword) + 1) * keyword
     rez = repeats[: len(plaintext)]
     index = 0
@@ -29,21 +28,6 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
                 if i.islower():
                     ciphertext += (chr(ord("a") + (ord(i) + shift - ord("z")) - 1)).lower()
                 index += 1
-=======
-    for char in range(len(plaintext)):
-        if plaintext[char].isupper():
-            new_ord = ord(plaintext[char]) + (ord(keyword[char % len(keyword)])-ord("A"))
-            if new_ord > ord("Z"):
-                new_ord -= 26
-            ciphertext += chr(new_ord)
-            
-        else:
-            new_ord = ord(plaintext[char]) + (ord(keyword[char % len(keyword)]) - ord("a"))
-            if new_ord > ord("z"):
-                new_ord -= 26
-            ciphertext += chr(new_ord)
-            
->>>>>>> a6f5bf7228dd900b0db3679dfa36982ba85e67c3
     return ciphertext
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
@@ -57,7 +41,6 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-<<<<<<< HEAD
     repeats = (len(ciphertext) // len(keyword) + 1) * keyword
     rez = repeats[: len(ciphertext)]
     index = 0
@@ -77,20 +60,4 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
                 if i.islower():
                     plaintext += (chr(ord(i) - ord("a") + ord("z") - shift + 1)).lower()
                 index += 1
-=======
-    
-    for char in range(len(ciphertext)):
-        if ciphertext[char].isupper():
-            new_ord = ord(ciphertext[char]) - (ord(keyword[char % len(keyword)]) - ord("A"))
-            if new_ord < ord("A"):
-                new_ord += 26
-            plaintext += chr(new_ord)
-            
-        else:
-            new_ord = ord(ciphertext[char]) + (ord(keyword[char % len(keyword)]) - ord("a"))
-            if new_ord < ord("a"):
-                new_ord += 26
-            plaintext += chr(new_ord)
-            
->>>>>>> a6f5bf7228dd900b0db3679dfa36982ba85e67c3
     return plaintext
