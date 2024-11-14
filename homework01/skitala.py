d90982a8a43d8eada636d6bf07a43fb8fc24f8f0
Мария Суханова <mariasukh2006@gmail.com>
2024-11-14 15:25:03 +0300

    Исправлены ошибки

--- homework01/skitala.py
+++ homework01/skitala.py
@@ -1,13 +1,4 @@
 def encrypt_scytale(plaintext, n):
-    """
-    Tests the encryption to scytale.
-    >>> encrypt_scytale("ПИТОН ЛУЧШИЙ", 3)
-    'ПОЛШИНУИТ_ЧЙ'
-    >>> encrypt_scytale("python is the best", 5)
-    'pntey_hstieths_o_b'
-    >>> encrypt_scytale("всем привет как дела", 7)
-    'виксв_еедмте__лпкара'
-    """
 
     plaintext = plaintext.replace(" ", "_")
     result = [""] * n
@@ -15,6 +6,6 @@ def encrypt_scytale(plaintext, n):
     for i in range(len(plaintext)):
         col = i % n
         result[col] += plaintext[i]
-    ciphertext = "".join(result)
+        ciphertext = "".join(result)
 
     return ciphertext
