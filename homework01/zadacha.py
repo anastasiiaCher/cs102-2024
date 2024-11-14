07072c7aa96f19a0007f480ff05cf7ffa13764d4
yunoori <liliakhaibullina0407@gmail.com>
2024-11-14 16:43:35 +0300

    задача

--- homework01/zadacha.py
+++ homework01/zadacha.py
@@ -2,7 +2,7 @@ def encrypt_transposition(plaintext, block_size, id1, id2):
     if id1 >= block_size or id2 >= block_size:
         raise ValueError("Индексы id1 и id2 должны быть меньше размера блока.")
     
-    blocks = [plaintext[i:i + block_size] for i in range(0, len(plaintext), block_size)]
+    blocks = [plaintext[i : i + block_size] for i in range(0, len(plaintext), block_size)]
     encrypted_blocks = []
 
     for block in blocks:
@@ -14,10 +14,10 @@ def encrypt_transposition(plaintext, block_size, id1, id2):
 
         block_list[id1], block_list[id2] = block_list[id2], block_list[id1]
 
-        encrypted_blocks.append(''.join(block_list))
+        encrypted_blocks.append("".join(block_list))
 
     
-    return ''.join(encrypted_blocks)
+    return "".join(encrypted_blocks)
 
 
 if __name__ == "__main__":
@@ -27,3 +27,4 @@ if __name__ == "__main__":
     id2 = 3
     encrypted_text = encrypt_transposition(plaintext, block_size, id1, id2)
     print(encrypted_text)
+    
