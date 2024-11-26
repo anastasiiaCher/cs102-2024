427eedca7e64fbc305afee7f376fee97e467f144
amina339 <unicorn23.ru@gmail.com>
2024-11-26 19:44:21 +0300

    Реализована функция group()

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -41,7 +41,10 @@ def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
     >>> group([1,2,3,4,5,6,7,8,9], 3)
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
-    pass
+    l = []
+    for i in range(0, len(values), n):
+        l.append(values[i : i + n])
+    return l
 
 
 def get_row(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
