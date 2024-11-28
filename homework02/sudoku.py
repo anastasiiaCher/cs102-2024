05b3fc9e3259720d63aa6e9a6f2e59aa43f97693
Arseny 467106 <arpleshnev@yandex.ru>
2024-11-28 12:12:11 +0300

    Implemented solve function

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -150,8 +150,24 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    pass
+    pos = find_empty_positions(grid)
+    if pos == None:
+        return grid
 
+    values = find_possible_values(grid, pos)
+    if values == None:
+        return None
+
+    y, x = pos
+    for n in values:
+        grid[y][x] = n
+        g = solve(grid)
+
+        if g == None:
+            grid[y][x] = "."
+            continue
+        else:
+            return g
 
 def check_solution(solution: tp.List[tp.List[str]]) -> bool:
     """ Если решение solution верно, то вернуть True, в противном случае False """
