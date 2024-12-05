595b5027dac7e052973fb2f0d7eee9efac6100f4
богдан баташов <skr1merbot@gmail.com>
2024-12-05 15:35:08 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,23 +1,28 @@
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    """
-    Encrypts plaintext using a Caesar cipher.
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
+    """Encrypting with Caesar cipher"""
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for char in plaintext:
+        if char.isalpha():
+            start = ord('A') if char.isupper() else ord('a')
+            shifted_char = chr((ord(char) - start + shift) % 26 + start)
+        else:
+            shifted_char = char
+        ciphertext += shifted_char
     return ciphertext
 
+texts = ["PYTHON", "python", "Python3.6", ""]
+shift = 3
+
+for text in texts:
+    encrypted_text = encrypt_caesar(text, shift)
+    print(f"Исходный текст: {text}")
+    print(f"Зашифрованный текст: {encrypted_text}")
+    print("-" * 20)
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     Decrypts a ciphertext using a Caesar cipher.
+
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
     >>> decrypt_caesar("sbwkrq")
@@ -28,5 +33,21 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for char in ciphertext:
+        if char.isalpha():
+            start = ord('A') if char.isupper() else ord('a')
+            shifted_char = chr((ord(char) - start - shift) % 26 + start)
+        else:
+            shifted_char = char
+        plaintext += shifted_char
+    return plaintext
+
+    texts = ["SBWKRQ", "sbwkrq", "Sbwkrq3.6", ""]
+    shift = 3
+
+    for text in texts:
+        decrypted_text = decrypt_caesar(text, shift)
+        print(f"Зашифрованный текст: {text}")
+        print(f"Дешифрованный текст: {decrypted_text}")
+        print("-" * 20)
     return plaintext
