e6aadca87f1d92fc2e166f339d89715b704a3f6b
dianuussss <umirbaevadiana89@gmail.com>
2024-12-06 20:12:52 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -8,9 +8,25 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
-    return ciphertext
+    if not plaintext:
+        return plaintext
+
+    ciphertext = []
+    keyword = (keyword.lower() * (len(plaintext) // len(keyword) + 1))[:len(plaintext)]
+
+    for i, letter in enumerate(plaintext):
+        shift = ord(keyword[i].lower()) - ord("a")
+
+        if "A" <= letter <= "Z":
+            new_letter = chr((ord(letter) - ord("A") + shift) % 26 + ord("A"))
+        elif "a" <= letter <= "z":
+            new_letter = chr((ord(letter) - ord("a") + shift) % 26 + ord("a"))
+        else:
+            new_letter = letter
+
+        ciphertext.append(new_letter)
+
+    return "".join(ciphertext)
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
@@ -23,6 +39,22 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    plaintext = ""
-    # PUT YOUR CODE HERE
-    return plaintext
+    if not ciphertext:
+        return ciphertext
+
+    plaintext = []
+    keyword = (keyword.lower() * (len(ciphertext) // len(keyword) + 1))[:len(ciphertext)]
+
+    for i, letter in enumerate(ciphertext):
+        shift = ord(keyword[i].lower()) - ord("a")
+
+        if "A" <= letter <= "Z":
+            new_letter = chr((ord(letter) - ord("A") - shift) % 26 + ord("A"))
+        elif "a" <= letter <= "z":
+            new_letter = chr((ord(letter) - ord("a") - shift) % 26 + ord("a"))
+        else:
+            new_letter = letter
+
+        plaintext.append(new_letter)
+
+    return "".join(plaintext)
