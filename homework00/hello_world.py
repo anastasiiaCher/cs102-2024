11b1ef33d52ce051a0a6a5f0ee2224d5224b88ca
macbook <macbook@MacBook-Pro-macbook-4.local>
2024-12-14 00:34:19 +0300

    Исправлены ошибки

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,2 +1,7 @@
-def text():
+"""This module contains function that prints 'Hello, World!'."""
+def text(message):
+    """Prints 'Hello, World!' to the console."""
     return message
+
+
+print(text("Hello, World!"))
