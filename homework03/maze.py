a86f8055c325e72b9abdd038e77ba08f64ecf2b5
Илона Бондар <ilona_bondars@mail.ru>
2024-12-14 09:29:46 +0300

    Файл изменен

--- homework03/maze.py
+++ homework03/maze.py
@@ -13,9 +13,7 @@ def create_grid(rows: int = 15, cols: int = 15) -> List[List[Union[str, int]]]:
     return [["■"] * cols for _ in range(rows)]
 
 
-def remove_wall(
-        grid: List[List[Union[str, int]]], coord: Tuple[int, int]
-        ) -> List[List[Union[str, int]]]:
+def remove_wall(grid: List[List[Union[str, int]]], coord: Tuple[int, int]) -> List[List[Union[str, int]]]:
     """
 
     :param grid:
@@ -37,9 +35,7 @@ def remove_wall(
     return grid
 
 
-def bin_tree_maze(
-        rows: int = 15, cols: int = 15, random_exit: bool = True
-        ) -> List[List[Union[str, int]]]:
+def bin_tree_maze(rows: int = 15, cols: int = 15, random_exit: bool = True) -> List[List[Union[str, int]]]:
     """
 
     :param rows:
@@ -61,12 +57,8 @@ def bin_tree_maze(
         remove_wall(grid, (x, y))
     if random_exit:
         x_in, x_out = randint(0, rows - 1), randint(0, rows - 1)
-        y_in = randint(0, cols - 1) if x_in in (0, rows - 1) else choice(
-            (0, cols - 1)
-            )
-        y_out = randint(0, cols - 1) if x_out in (0, rows - 1) else choice(
-            (0, cols - 1)
-            )
+        y_in = randint(0, cols - 1) if x_in in (0, rows - 1) else choice((0, cols - 1))
+        y_out = randint(0, cols - 1) if x_out in (0, rows - 1) else choice((0, cols - 1))
     else:
         x_in, y_in = 0, cols - 2
         x_out, y_out = rows - 1, 1
@@ -83,13 +75,10 @@ def get_exits(grid: List[List[Union[str, int]]]) -> List[Tuple[int, int]]:
     :return:
     """
 
-    return [(x, y) for x, row in enumerate(grid) for y, if_exit in enumerate(
-        row) if if_exit == "X"]
+    return [(x, y) for x, row in enumerate(grid) for y, if_exit in enumerate(row) if if_exit == "X"]
 
 
-def make_step(
-    grid: List[List[Union[str, int]]], k: int
-) -> List[List[Union[str, int]]]:
+def make_step(grid: List[List[Union[str, int]]], k: int) -> List[List[Union[str, int]]]:
     """
 
     :param grid:
@@ -97,8 +86,7 @@ def make_step(
     :return:
     """
 
-    coords = [(x, y) for x, row in enumerate(grid) for y, _ in enumerate(
-        row) if grid[x][y] == k]
+    coords = [(x, y) for x, row in enumerate(grid) for y, _ in enumerate(row) if grid[x][y] == k]
 
     k += 1
     for i in coords:
