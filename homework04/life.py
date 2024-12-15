3cff18fb0ca25bd572fcb443ce8db1610ee468e5
elkulakova <elizaveta.kulakova@niuitmo.ru>
2024-12-15 19:12:12 +0300

    Написана логика игры Жизнь и ее прототип

--- homework04/life.py
+++ homework04/life.py
@@ -1,9 +1,10 @@
+""" "Game of Life" game implementation based on suggestions from the course """
+
 import pathlib
 import random
 import typing as tp
 
 import pygame
-from pygame.locals import *
 
 Cell = tp.Tuple[int, int]
 Cells = tp.List[int]
@@ -11,6 +12,7 @@ Grid = tp.List[Cells]
 
 
 class GameOfLife:
+    """Creating Game of Life class"""
     def __init__(
         self,
         size: tp.Tuple[int, int],
@@ -19,6 +21,7 @@ class GameOfLife:
     ) -> None:
         # Размер клеточного поля
         self.rows, self.cols = size
+        self.cell_size = 10
         # Предыдущее поколение клеток
         self.prev_generation = self.create_grid()
         # Текущее поколение клеток
@@ -29,46 +32,77 @@ class GameOfLife:
         self.generations = 1
 
     def create_grid(self, randomize: bool = False) -> Grid:
-        # Copy from previous assignment
-        pass
+        """Creating a grid"""
+        if not randomize:
+            grid = [[0] * self.cols for _ in range(self.rows)]
+        else:
+            grid = [[random.randint(0, 1) for _ in range(self.cols)] for _ in range(self.rows)]
+        return grid
 
     def get_neighbours(self, cell: Cell) -> Cells:
-        # Copy from previous assignment
-        pass
+        """Getting the condition of neighbours"""
+        x, y = cell
+        neib_cells = []
+        for i in range(max(0, x - 1), min(self.rows, x + 2)):
+            for j in range(max(0, y - 1), min(self.cols, y + 2)):
+                if i == x and j == y:
+                    continue
+                neib_cells.append(self.curr_generation[i][j])
+        return neib_cells
 
     def get_next_generation(self) -> Grid:
-        # Copy from previous assignment
-        pass
+        """Getting the next generation"""
+        new_grid = [[0] * self.cols for _ in range(self.rows)]
+        for i in range(self.rows):
+            for j in range(self.cols):
+                neib_cells = self.get_neighbours((i, j))
+                alive_neibs = sum(neib_cells)
+                if self.curr_generation[i][j] == 0 and alive_neibs == 3:
+                    new_grid[i][j] = 1
+                elif self.curr_generation[i][j] == 1:
+                    if 2 <= alive_neibs <= 3:
+                        new_grid[i][j] = 1
+        return new_grid
 
     def step(self) -> None:
         """
-        Выполнить один шаг игры.
+        Perform one step of the game.
         """
-        pass
+        if not self.is_max_generations_exceeded:
+            self.prev_generation = self.curr_generation
+            self.curr_generation = self.get_next_generation()
+            if self.is_changing:
+                self.generations += 1
+        else:
+            pygame.quit() # pylint: disable=no-member
 
     @property
     def is_max_generations_exceeded(self) -> bool:
         """
         Не превысило ли текущее число поколений максимально допустимое.
         """
-        pass
+        return self.max_generations is not None and self.generations >= self.max_generations
 
     @property
     def is_changing(self) -> bool:
         """
         Изменилось ли состояние клеток с предыдущего шага.
         """
-        pass
+        return self.prev_generation != self.curr_generation
 
     @staticmethod
     def from_file(filename: pathlib.Path) -> "GameOfLife":
         """
         Прочитать состояние клеток из указанного файла.
         """
-        pass
+        with open(filename, encoding='utf-8') as f:
+            grid = [[int(cell) for cell in line] for line in f]
+        return GameOfLife((len(grid), len(grid[0])))
 
     def save(self, filename: pathlib.Path) -> None:
         """
         Сохранить текущее состояние клеток в указанный файл.
         """
-        pass
+        with open(filename, "w", encoding='utf-8') as f:
+            for line in self.curr_generation:
+                f.write("".join(str(cell) for cell in line))
--- homework04/life_proto.py
+++ homework04/life_proto.py
@@ -1,8 +1,10 @@
+"""Creating "Game of Life" prototype"""
+
 import random
 import typing as tp
 
 import pygame
-from pygame.locals import *
+from pygame.locals import QUIT
 
 Cell = tp.Tuple[int, int]
 Cells = tp.List[int]
@@ -10,9 +12,9 @@ Grid = tp.List[Cells]
 
 
 class GameOfLife:
-    def __init__(
-        self, width: int = 640, height: int = 480, cell_size: int = 10, speed: int = 10
-    ) -> None:
+    """Game of Life proto class"""
+
+    def __init__(self, width: int = 640, height: int = 480, cell_size: int = 10, speed: int = 10) -> None:
         self.width = width
         self.height = height
         self.cell_size = cell_size
@@ -29,22 +31,22 @@ class GameOfLife:
         # Скорость протекания игры
         self.speed = speed
 
+        # Создание списка клеток
+        self.grid = self.create_grid(randomize=True)
+
     def draw_lines(self) -> None:
-        """ Отрисовать сетку """
+        """Отрисовать сетку"""
         for x in range(0, self.width, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
         for y in range(0, self.height, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
 
     def run(self) -> None:
-        """ Запустить игру """
-        pygame.init()
+        """Запустить игру"""
+        pygame.init()  # pylint: disable=no-member
         clock = pygame.time.Clock()
         pygame.display.set_caption("Game of Life")
         self.screen.fill(pygame.Color("white"))
-
-        # Создание списка клеток
-        # PUT YOUR CODE HERE
 
         running = True
         while running:
@@ -55,11 +57,10 @@ class GameOfLife:
 
             # Отрисовка списка клеток
             # Выполнение одного шага игры (обновление состояния ячеек)
-            # PUT YOUR CODE HERE
-
+            self.draw_grid()
             pygame.display.flip()
             clock.tick(self.speed)
-        pygame.quit()
+        pygame.quit()  # pylint: disable=no-member
 
     def create_grid(self, randomize: bool = False) -> Grid:
         """
@@ -79,13 +80,22 @@ class GameOfLife:
         out : Grid
             Матрица клеток размером `cell_height` х `cell_width`.
         """
-        pass
+        if not randomize:
+            grid = [[0] * self.cell_width for _ in range(self.cell_height)]
+        else:
+            grid = [[random.randint(0, 1) for _ in range(self.cell_width)] for _ in range(self.cell_height)]
+        return grid
 
     def draw_grid(self) -> None:
         """
         Отрисовка списка клеток с закрашиванием их в соответствующе цвета.
         """
-        pass
+        for i in range(self.cell_height):
+            for j in range(self.cell_width):
+                color = pygame.Color("white") if self.grid[i][j] == 0 else pygame.Color("green")
+                pygame.draw.rect(
+                    self.screen, color, (j * self.cell_size, i * self.cell_size, self.cell_size, self.cell_size)
+                )
 
     def get_neighbours(self, cell: Cell) -> Cells:
         """
@@ -105,7 +115,14 @@ class GameOfLife:
         out : Cells
             Список соседних клеток.
         """
-        pass
+        x, y = cell
+        neib_cells = []
+        for i in range(max(0, x - 1), min(self.cell_height, x + 2)):
+            for j in range(max(0, y - 1), min(self.cell_width, y + 2)):
+                if i == x and j == y:
+                    continue
+                neib_cells.append(self.grid[i][j])
+        return neib_cells
 
     def get_next_generation(self) -> Grid:
         """
@@ -116,4 +133,14 @@ class GameOfLife:
         out : Grid
             Новое поколение клеток.
         """
-        pass
+        new_grid = [[0] * self.cell_width for _ in range(self.cell_height)]
+        for i in range(self.cell_height):
+            for j in range(self.cell_width):
+                neib_cells = self.get_neighbours((i, j))
+                alive_neibs = sum(neib_cells)
+                if self.grid[i][j] == 0 and alive_neibs == 3:
+                    new_grid[i][j] = 1
+                elif self.grid[i][j] == 1:
+                    if 2 <= alive_neibs <= 3:
+                        new_grid[i][j] = 1
+        return new_grid
