d76f8448a186ca0b016200bbd740bd1efcdcc46f
unknown <cooperslempy@gmail.com>
2024-12-20 02:50:46 +0300

    вигенер коммит

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,6 +1,7 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
+
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> encrypt_vigenere("python", "a")
@@ -9,13 +10,36 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    key_length = len(keyword)
+    key_index = 0  # Индекс для перебора символов ключа
+
+    for char in plaintext:
+        if char.isalpha():
+            # Определяем сдвиг
+            # Приводим символ ключа к верхнему регистру для удобства вычислений
+            key_char = keyword[key_index % key_length].upper()
+            shift = ord(key_char) - ord('A')
+
+            if char.isupper():
+                # Шифруем заглавную букву
+                new_char = chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+            else:
+                # Шифруем строчную букву
+                new_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+
+            ciphertext += new_char
+            key_index += 1
+        else:
+            # Не меняем неалфавитные символы
+            ciphertext += char
+
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
+
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> decrypt_vigenere("python", "a")
@@ -24,5 +48,26 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    key_length = len(keyword)
+    key_index = 0  # Индекс для перебора символов ключа
+
+    for char in ciphertext:
+        if char.isalpha():
+            # Определяем сдвиг
+            key_char = keyword[key_index % key_length].upper()
+            shift = ord(key_char) - ord('A')
+
+            if char.isupper():
+                # Дешифруем заглавную букву
+                new_char = chr((ord(char) - ord('A') - shift) % 26 + ord('A'))
+            else:
+                # Дешифруем строчную букву
+                new_char = chr((ord(char) - ord('a') - shift) % 26 + ord('a'))
+
+            plaintext += new_char
+            key_index += 1
+        else:
+            # Не меняем неалфавитные символы
+            plaintext += char
+
     return plaintext
