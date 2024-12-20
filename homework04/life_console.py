8dc2c177a9caf695adfd8744dc0d5af9750259df
zalof <zalof299+unigithub@gmail.com>
2024-12-20 06:30:29 +0300

    Finished life_console.py

--- homework04/life_console.py
+++ homework04/life_console.py
@@ -10,13 +10,26 @@ class Console(UI):
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        pass
+        screen.border(0)
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
-        pass
+        max_y, max_x = screen.getmaxyx()
+        for y, row in enumerate(self.life.curr_generation):
+            for x, cell in enumerate(row):
+                if y < max_y and x < max_x:
+                    screen.addch(y, x, "#" if cell else ".")
 
     def run(self) -> None:
         screen = curses.initscr()
-        # PUT YOUR CODE HERE
+
+        while self.life.is_changing and not self.life.is_max_generations_exceeded:
+            screen.clear()
+            self.draw_borders(screen)
+            self.draw_grid(screen)
+            screen.refresh()
+
+            self.life.step()
+            
+            
         curses.endwin()
